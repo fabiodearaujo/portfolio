f6e0009e9b912e843cdc934e50ec3f087f31b9ae
fabiodearaujo <96082041+fabiodearaujo@users.noreply.github.com>
2022-07-27 01:05:07 +0100

    finalized Skill section and created Projects section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <Navbar/>
       <Hero/>
       <Skills/>
+      <Projects/>
     </div>
   )
 }
