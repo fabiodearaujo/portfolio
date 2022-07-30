34db0906ebd0df702bc7cf179fe8af96193cb81e
fabiodearaujo <96082041+fabiodearaujo@users.noreply.github.com>
2022-07-30 12:52:29 +0100

    fixing skills buttons background not showing properly on iPhone and safari

--- components/SkillItem.jsx
+++ components/SkillItem.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
 
-const SkillItem = ({skillURL, backgroundImage, title}) => {
+const SkillItem = ({skillURL, backgroundImage, title, decoration}) => {
   return (
-    <div className='relative m-2 px-20 py-7 rounded-2xl shadow-gray-900 shadow-md  hover:scale-105 ease-in duration-300 cursor-pointer'>
-      <a href={skillURL} target='_blank' rel='noopener noreferrer'>
-        <Image
-            className="rounded-2xl"
-            src={backgroundImage}
-            alt={title}
-            layout='fill'
-        />
-      </a>
-    </div>
+      <div className={decoration}>
+        <a href={skillURL} target='_blank' rel='noopener noreferrer'>
+          <Image
+              className="rounded-2xl"
+              src={backgroundImage}
+              alt={title}
+              layout='fill'
+          />
+        </a>
+      </div>
   )
 }
 
